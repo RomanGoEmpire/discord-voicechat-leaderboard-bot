4bffd2495fadead42cf3d325e9f28991b90a6fb5
RomanGoEmpire <71276897+RomanGoEmpire@users.noreply.github.com>
2023-11-25 02:06:59 +0100

    Add join time tracking for voice channel

--- bot.py
+++ bot.py
@@ -1,10 +1,16 @@
 # This example requires the 'message_content' intent.
 
+import datetime
+
 import discord
 from decouple import config
 
 
 class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.join_times = {}
+
     async def on_ready(self):
         print(f"Logged on as {self.user}!")
 
@@ -13,9 +19,15 @@ class MyClient(discord.Client):
 
     async def on_voice_state_update(self, member, before, after):
         if before.channel is None and after.channel is not None:
+            self.join_times[member.id] = datetime.datetime.now()
             print(f"{member} has joined a voice channel.")
         elif before.channel is not None and after.channel is None:
-            print(f"{member} has left a voice channel.")
+            if member.id in self.join_times:
+                duration = datetime.datetime.now() - self.join_times[member.id]
+                del self.join_times[member.id]
+                print(
+                    f"{member} has left a voice channel. They were in the channel for {duration.total_seconds()} seconds."
+                )
 
 
 intents = discord.Intents.default()
