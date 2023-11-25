62c840a6418af671268045885abb71d2b131650f
RomanGoEmpire <71276897+RomanGoEmpire@users.noreply.github.com>
2023-11-25 02:26:28 +0100

    Refactor code and add database functionality

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
-# This example requires the 'message_content' intent.
-
 import datetime
 
 import discord
 from decouple import config
 
+from database import Database
 
-class MyClient(discord.Client):
+
+class Bot(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.join_times = {}
+        self.db = Database("discord.db")
 
     async def on_ready(self):
         print(f"Logged on as {self.user}!")
@@ -18,22 +19,35 @@ class MyClient(discord.Client):
         print(f"Message from {message.author}: {message.content}")
 
     async def on_voice_state_update(self, member, before, after):
+        # If the member was connected to a voice channel before
         if before.channel is None and after.channel is not None:
             self.join_times[member.id] = datetime.datetime.now()
             print(f"{member} has joined a voice channel.")
+
         elif before.channel is not None and after.channel is None:
             if member.id in self.join_times:
                 duration = datetime.datetime.now() - self.join_times[member.id]
+                duration = int(duration.total_seconds())
                 del self.join_times[member.id]
+
+                if not self.db.user_exists(member.id):
+                    print(f"Adding {member} to the database.")
+                    self.db.insert_user(member.id, member.name)
+                self.db.insert_user_activity(
+                    member.id,
+                    duration,
+                    datetime.datetime.now().strftime("%Y-%m-%d"),
+                    self.join_times[member.id].strftime("%H:%M:%S"),
+                    datetime.datetime.now().strftime("%H:%M:%S"),
+                )
                 print(
-                    f"{member} has left a voice channel. They were in the channel for {duration.total_seconds()} seconds."
+                    f"{member} has left a voice channel. They were in the channel for {duration} seconds."
                 )
 
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
-key = config["api-key"]
-print(key)
+client = Bot(intents=intents)
+key = config("api_key")
 client.run(key)
--- database.py
+++ database.py
@@ -7,9 +7,11 @@ class Database:
         self.cursor = self.conn.cursor()
 
     def create_table(self):
+        self.cursor.execute("DROP TABLE IF EXISTS users")
         self.cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username TEXT NOT NULL
             )
         """
@@ -17,6 +19,7 @@ class Database:
         self.conn.commit()
 
     def create_user_activity_table(self):
+        self.cursor.execute("DROP TABLE IF EXISTS user_activity")
         self.cursor.execute(
             """
             CREATE TABLE IF NOT EXISTS user_activity (
@@ -31,10 +34,37 @@ class Database:
         )
         self.conn.commit()
 
+    def user_exists(self, id):
+        self.cursor.execute(
+            """SELECT * FROM users WHERE id = ?""",
+            (id,),
+        )
+        return self.cursor.fetchone() is not None
+
+    def insert_user(self, id, username):
+        self.cursor.execute(
+            """
+            INSERT INTO users (id, username) VALUES (?, ?)
+        """,
+            (id, username),
+        )
+        self.conn.commit()
+
+    def insert_user_activity(self, user_id, duration, date, joining_time, leaving_time):
+        self.cursor.execute(
+            """
+            INSERT INTO user_activity (user_id, duration, date, joining_time, leaving_time) VALUES (?, ?, ?, ?, ?)
+        """,
+            (user_id, duration, date, joining_time, leaving_time),
+        )
+        self.conn.commit()
+
     def close(self):
         self.conn.close()
 
 
-db = Database("discord.db")
-db.create_table()
-db.close()
+if __name__ == "__main__":
+    db = Database("discord.db")
+    db.create_table()
+    db.create_user_activity_table()
+    db.close()
