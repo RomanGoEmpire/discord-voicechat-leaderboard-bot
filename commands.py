20b250569a1f27a71c76647b7b3945357b74986b
Roman Gerloff <71276897+RomanGoEmpire@users.noreply.github.com>
2024-01-17 14:01:41 +0100

    Remove unnecessary code in leader board display

--- commands.py
+++ commands.py
@@ -113,8 +113,6 @@ class Commands(commands.Cog):
         others = ""
         count = 0
         for rank, (user_id, duration) in enumerate(leader_board, start=1):
-            if count == 15:
-                break
             member = await ctx.guild.fetch_member(user_id)
             if member:
                 display_name = member.display_name
